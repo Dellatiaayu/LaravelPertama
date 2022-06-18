1169a1278c162c1f32c3bc948f3b9ca4505017ba
Dellatiaayu <dellatiaayu@gmail.com>
2022-06-18 17:11:05 +0700

    commit9

---
+++ app/Http/Controllers/Api/CobaController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+
+use App\Models\Friends;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CobaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $friends = Friends::orderBy('id', 'desc')->get();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Daftar Data Teman',
+            'data' => $friends
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
